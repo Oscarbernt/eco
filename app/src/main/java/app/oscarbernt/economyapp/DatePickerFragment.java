02218ee2d9b2e06b6e264e1362ca08bc7dcb3748
Oscar <oscarbernt@gmail.com>
2015-03-08 16:28:41 +0100

    Hej

--- app/src/main/java/app/oscarbernt/economyapp/DatePickerFragment.java
+++ app/src/main/java/app/oscarbernt/economyapp/DatePickerFragment.java
@@ -1,14 +1,13 @@
 package app.oscarbernt.economyapp;
 
 
-import android.annotation.TargetApi;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.widget.DatePicker;
 import java.util.Calendar;
-@TargetApi(11)
+
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener{
 
     @Override
